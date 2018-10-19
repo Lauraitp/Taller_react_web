b49ac6e61d1f216667276807e18d075bc4bf92f1
LauraITP <laura.tabares@correo.icesi.edu.co>
2018-10-18 19:19:36 -0500

    upload docs folder

---
+++ docs/precache-manifest.3121fd1e38f2dca96e81e9b8da99e465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Taller_react_web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "981ad9f3aeb39500d254",
+    "url": "/Taller_react_web/static/js/runtime~main.981ad9f3.js"
+  },
+  {
+    "revision": "764d7c0e61694a04ba3a",
+    "url": "/Taller_react_web/static/js/main.764d7c0e.chunk.js"
+  },
+  {
+    "revision": "5511a812470eeb82e813",
+    "url": "/Taller_react_web/static/js/1.5511a812.chunk.js"
+  },
+  {
+    "revision": "764d7c0e61694a04ba3a",
+    "url": "/Taller_react_web/static/css/main.b641fe38.chunk.css"
+  },
+  {
+    "revision": "a89616455b043f0da8ba13f66a56aba1",
+    "url": "/Taller_react_web/index.html"
+  }
+];
---
+++ docs/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/Taller_react_web/precache-manifest.3121fd1e38f2dca96e81e9b8da99e465.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Taller_react_web/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
